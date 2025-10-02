efc9bb8ac5d2480ef6422e7ff1afb1f5c9177c10
kasemir <ky9@ornl.gov>
2025-10-02 16:49:48 -0400

    jdk

--- setup/01_prereq.sh
+++ setup/01_prereq.sh
@@ -1,8 +1,15 @@
 # For epics base
 sudo dnf install -y perl
+sudo dnf install -y readline-devel
 
 # For sequencer
 sudo dnf install -y re2c
+
+# For CSS
+sudo dnf install -y java-21-openjdk-devel
+
+# Stuff
+sudo dnf install -y htop
 
 # Shell settings
 sudo cp $HOME/training/tools/training_profile.sh /etc/profile.d
--- setup/10_jdk_css.sh
+++ setup/10_jdk_css.sh
@@ -1,27 +1,4 @@
-echo "== JDK"
 cd $HOME/training/tools
-
-rm -rf jdk
-
-# For VM on newer Mac, need a aarch64 version, otherwise x64
-# Hard to find durable download links
-if [[ `uname -m` == "aarch64" ]]
-then
-    wget https://download.java.net/java/GA/jdk25/bd75d5f9689641da8e1daabeccb5528b/36/GPL/openjdk-25_linux-aarch64_bin.tar.gz
-    #wget https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.7%2B6/OpenJDK21U-jdk_aarch64_linux_hotspot_21.0.7_6.tar.gz
-else
-    wget wget https://download.java.net/java/GA/jdk25/bd75d5f9689641da8e1daabeccb5528b/36/GPL/openjdk-25_linux-x64_bin.tar.gz
-fi
-
-tar zxf openjdk*
-rm openjdk*
-mv jdk-* jdk
-
-export JAVA_HOME=$HOME/training/tools/jdk
-export PATH="$JAVA_HOME/bin:$PATH"
-
-java -version
-
 
 echo "== MVN"
 rm -rf apache-maven
@@ -33,7 +10,6 @@ mv apache-maven-* apache-maven
 export M2_HOME=$HOME/training/tools/apache-maven
 export PATH="$M2_HOME/bin:$PATH"
 mvn -version
-
 
 
 echo "== CS-Studio phoebus"
