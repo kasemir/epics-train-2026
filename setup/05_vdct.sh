d606014f184e7a44755237fb2531d7ef12e7147e
epics-dev <epics-dev@unregistered.ornl.gov>
2025-10-03 11:27:03 -0400

    vdct

---
+++ setup/05_vdct.sh
@@ -0,0 +1,6 @@
+cd ~/training/tools
+rm -rf vdct
+wget https://github.com/epics-extensions/VisualDCT/releases/download/v2.8.3/VisualDCT-2.8.3-distribution.tar.gz
+tar vzxf VisualDCT-*-distribution.tar.gz
+rm VisualDCT-*-distribution.tar.gz
+mv VisualDCT-* vdct
