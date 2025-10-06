e17120077a5f1159c69754b8457a22f6ee628e32
epics-dev <epics-dev@unregistered.ornl.gov>
2025-10-06 14:20:00 -0400

    Autosave prep for lab

--- examples/06_autosave/iocBoot/iocdemo/st.cmd
+++ examples/06_autosave/iocBoot/iocdemo/st.cmd
@@ -15,18 +15,18 @@ dbLoadRecords("db/autosave.db","S=demo")
 
 ### AUTOSAVE: Configure ###############################
 # File locations, using some macros
-epicsEnvSet("SAVE_DIR", "${TOP}/iocBoot/${IOC}")
-save_restoreSet_status_prefix("demo:")
-set_requestfile_path("$(SAVE_DIR)")
-set_savefile_path("$(SAVE_DIR)")
+#epicsEnvSet("SAVE_DIR", "${TOP}/iocBoot/${IOC}")
+#save_restoreSet_status_prefix("demo:")
+#set_requestfile_path("$(SAVE_DIR)")
+#set_savefile_path("$(SAVE_DIR)")
 
 # Schedule a maximum of 3 sequenced backups of the .sav file
 # every 10 minutes - .sav0, .sav1, .sav2
-save_restoreSet_NumSeqFiles(3)
-save_restoreSet_SeqPeriodInSeconds(600)
+#save_restoreSet_NumSeqFiles(3)
+#save_restoreSet_SeqPeriodInSeconds(600)
 
 # Arrange for restoring saved values into records
-set_pass1_restoreFile("$(IOC).sav")
+#set_pass1_restoreFile("$(IOC).sav")
 ######################################################
 
 
@@ -35,7 +35,7 @@ iocInit()
 
 
 ### AUTOSAVE: Create request file and start periodic 'save’ ##
-makeAutosaveFileFromDbInfo("$(SAVE_DIR)/$(IOC).req", "autosaveFields")
-create_monitor_set("$(IOC).req", 5)
+#makeAutosaveFileFromDbInfo("$(SAVE_DIR)/$(IOC).req", "autosaveFields")
+#create_monitor_set("$(IOC).req", 5)
 ######################################################
 
