a9fa0dde4bf21361da2f0df192982dd7071bad6c
epics-dev <epics-dev@unregistered.ornl.gov>
2025-10-13 13:46:22 -0400

    AD

---
+++ examples/AreaDetector/start_sim_ioc.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd /ics/training/tools/ADSimDetector/iocs/simDetectorIOC/iocBoot/iocSimDetector
+
+./start_epics
+
--- setup/12_areadetector.sh
+++ setup/12_areadetector.sh
@@ -1,41 +1,44 @@
 echo "== Area Detector"
 
-sudo dnf install -y zlib-devel
+sudo dnf install libxml2-devel
+sudo dnf install zlib-devel
 
 cd /ics/training/tools
 
-wget https://github.com/areaDetector/areaDetector/archive/refs/tags/R3-11.zip
-unzip R3-11.zip
-rm R3-11.zip
-mv areaDetector-R3-11 areaDetector
-
-wget https://github.com/areaDetector/ADCore/archive/refs/tags/R3-11.zip
-unzip R3-11.zip
-mv ADCore-R3-11/* areaDetector/ADCore
-rm R3-11.zip
-rm -rf ADCore-R3-11
-
-wget https://github.com/areaDetector/ADSupport/archive/refs/tags/R1-10.zip
-unzip R1-10.zip
-mv ADSupport-R1-10/* areaDetector/ADSupport/
-rm -rf ADSupport-R1-10
-rm R1-10.zip
-
-wget https://github.com/areaDetector/ADSimDetector/archive/R2-10.zip
-unzip R2-10.zip 
-mv ADSimDetector-R2-10/* areaDetector/ADSimDetector/
-rm -rf ADSimDetector-R2-10/
-rm R2-10.zip 
-
-wget https://github.com/areaDetector/pvaDriver/archive/refs/tags/R1-5.zip
-unzip R1-5.zip
-rm R1-5.zip
-mv pvaDriver-R1-5/* areaDetector/pvaDriver/
-rm -rf pvaDriver-R1-5
-
-cd areaDetector
-(cd configure; ./copyFromExample)
-cp /ics/training/setup/areadetector_configure/* configure
-
+rm -rf ADCore
+wget https://github.com/areaDetector/ADCore/archive/refs/tags/R3-13.zip
+unzip R3-13.zip
+mv ADCore-R3-13 ADCore
+rm R3-13.zip
+cd ADCore
+echo 'include $(TOP)/../RELEASE.local' >> configure/RELEASE
+cp /ics/training/setup/areadetector_configure/CONFIG_SITE               configure
+cp /ics/training/setup/areadetector_configure/commonPlugins.cmd         iocBoot
+cp /ics/training/setup/areadetector_configure/commonPlugin_settings.req iocBoot
 make
+echo ADCORE=`pwd` >>../RELEASE.local
+cd ..
+
+rm -rf ADSimDetector
+wget https://github.com/areaDetector/ADSimDetector/archive/R2-11.zip
+unzip R2-11.zip 
+mv ADSimDetector-R2-11 ADSimDetector
+rm R2-11.zip 
+cd ADSimDetector
+echo 'include $(TOP)/../RELEASE.local' >> configure/RELEASE
+echo 'include $(TOP)/../../../RELEASE.local' >> iocs/simDetectorIOC/configure/RELEASE
+if [ `uname -m` == 'aarch64' ]
+then
+   sed -i 's/linux-x86_64/linux-aarch64/' iocs/simDetectorIOC/iocBoot/iocSimDetector/Makefile
+   sed -i 's/linux-x86_64/linux-aarch64/' iocs/simDetectorIOC/iocBoot/iocSimDetector/start_epics
+fi
+make
+make -C iocs/simDetectorIOC
+cp iocs/simDetectorIOC/iocBoot/iocSimDetector/envPaths iocs/simDetectorIOC/iocBoot/iocSimDetector/envPaths.linux
+
+#wget https://github.com/areaDetector/pvaDriver/archive/refs/tags/R1-5.zip
+#unzip R1-5.zip
+#rm R1-5.zip
+#mv pvaDriver-R1-5/* areaDetector/pvaDriver/
+#rm -rf pvaDriver-R1-5
 
