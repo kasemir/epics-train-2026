b21a6fc4654199ff3a7d38ac75c53e3c11b0e46f
epics-dev <epics-dev@unregistered.ornl.gov>
2025-10-17 11:58:33 -0400

    asyn TIRPC=NO on linux-x86_64

--- setup/06_asyn.sh
+++ setup/06_asyn.sh
@@ -4,13 +4,16 @@ rm -rf asyn
 
 # Earlier releases need rpcgen and libtirpc-devel, which
 # fails to install for Rocky 9.6?!
-# Latest as of Oct. 2025 defaults to skipping VXI11/RPC
+# Latest as of Oct. 2025 defaults to skipping VXI11/RPC,
+# except for linux-x86_64, so patch that to TIRPC=NO
 wget https://github.com/epics-modules/asyn/archive/refs/heads/master.zip
 unzip master.zip 
 rm master.zip 
 mv asyn-master asyn
 cd asyn/
 sed -i 's/^SUPPORT=/# SUPPORT=/' configure/RELEASE
+sed -i 's/TIRPC=YES/TIRPC=NO/' configure/CONFIG_SITE.Common.linux-x86_64
+
 make
 echo "ASYN=`pwd`" >> ../RELEASE.local
 
