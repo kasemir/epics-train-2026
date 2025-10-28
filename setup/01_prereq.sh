5e17a41b921907f18ad42b38ec396e0973f261c2
epics-dev <epics-dev@unregistered.ornl.gov>
2025-10-28 13:21:02 -0400

    desktop

--- setup/01_prereq.sh
+++ setup/01_prereq.sh
@@ -36,3 +36,12 @@ sudo dnf install -y htop
 # Shell settings
 sudo cp /ics/setup/training_profile.sh /etc/profile.d
 
+# Desktop
+sudo dnf install -y gnome-tweaks
+gsettings set org.gnome.desktop.background picture-uri file:////ics/EPICS.png
+gsettings set org.gnome.desktop.background picture-options scaled
+# gsettings set org.gnome.desktop.background picture-options centered
+gsettings set org.gnome.desktop.wm.preferences action-middle-click-titlebar 'lower'
+gsettings set org.gnome.desktop.wm.preferences action-right-click-titlebar 'menu'
+gsettings set org.gnome.desktop.wm.preferences action-double-click-titlebar 'toggle-maximize'
+
