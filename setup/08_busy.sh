fe6a759a442b22268ad6cc2ef57a3ce77784c995
kasemir <ky9@ornl.gov>
2026-01-29 12:19:00 -0500

    Busy 1.7.4

--- setup/08_busy.sh
+++ setup/08_busy.sh
@@ -2,10 +2,10 @@ echo "== Busy"
 cd /ics/tools/
 rm -rf busy
 
-wget https://epics.anl.gov/bcda/synApps/tar/busy_R1-6-1.tar.gz
-tar vzxf busy_R1-6-1.tar.gz
-rm busy_R1-6-1.tar.gz
-mv busy-1-6-1 busy
+wget https://github.com/epics-modules/busy/archive/refs/tags/R1-7-4.zip
+unzip R1-7-4.zip
+rm R1-7-4.zip
+mv busy-R* busy
 
 cd busy
 echo '-include $(TOP)/../RELEASE.local' >configure/RELEASE
